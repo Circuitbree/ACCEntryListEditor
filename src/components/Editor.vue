63256da8883d0db425405600cc9451889759ca99
Circuitbree <y.n.oniel12@gmail.com>
2021-11-14 12:42:32 +0100

    buttons above lists

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="container row">
+  <div class="row controls">
+    <div class="margin-bottom-2">
+      <button class="btn btn-secondary button" @click="$refs.editList.reset()">
+        Reset
+      </button>
+      <button class="btn btn-secondary button" @click="$refs.editList.reverse()">
+        Reverse
+      </button>
+      <a id="download" download="entrylist.json" class="btn btn-secondary button" @mousedown="$refs.editList.download()">
+        <i class="fa fa-download" aria-hidden="true"></i> Download
+      </a>
+    </div>
+  </div>
+  <div class="row">
     <div class="col-6">
       <ul class="list-group">
         <li class="list-group-item" v-for="element in qResults['entries']" :key="element">
-          <Driver :driverData="element" :showDropdown="false"></Driver>
+          <Driver :driverData="element" :showMenu="false"></Driver>
         </li>
       </ul>
     </div>
     <div class="col-6">
-      <Driverlist :initialList="qResults" :showControls="true" :disableDragging="false"/>
+      <Driverlist ref="editList" :initialList="qResults" :disableDragging="false"/>
     </div>
   </div>
 </template>
@@ -27,3 +40,14 @@
     },
   }
 </script>
+
+<style scoped>
+  .margin-bottom-2 {
+    margin-bottom: 2%;
+  }
+
+  .controls .btn {
+    margin: 1%;
+    width: 30%;
+  }
+</style>
